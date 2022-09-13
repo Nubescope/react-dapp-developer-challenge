554d05aa26fb2ca8ddd00b889628fc859065fe6e
Ignacio Olaciregui <ignacioola@gmail.com>
2022-09-13 12:20:19 -0300

    Remove comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,16 @@
 import cdaiAbi from '../assets/abi/cdai.json'
 import daiAbi from '../assets/abi/dai.json'
 
-// Goerli
-// const DAI_ADDRESS = '0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60'
-// const CDAI_ADDRESS = '0x822397d9a55d0fefd20f5c4bcab33c5f65bd28eb'
+/**
+ * Rinkeby Addresses
+ */
 
-// Rinkeby
 const DAI_ADDRESS = '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa'
 const CDAI_ADDRESS = '0x6D7F0754FFeb405d23C51CE938289d4835bE3b14'
+
+/**
+ * Exports
+ */
 
 export const CDAI = {
   address: CDAI_ADDRESS,
