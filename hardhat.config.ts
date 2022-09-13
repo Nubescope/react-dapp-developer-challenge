7b5033f2265ff95709e3ce7355ccfe5229e85d92
Ignacio Olaciregui <ignacioola@gmail.com>
2022-09-13 14:35:16 -0300

    Remove comment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,6 @@ import '@nomiclabs/hardhat-ethers'
 
 import ensureEnv from './src/helpers/ensureEnv'
 
-// const INFURA_KEY = '674b516cea814fc79d06e5e917ba1a11'
 require('dotenv').config()
 
 const INFURA_API_KEY = ensureEnv('INFURA_API_KEY')
