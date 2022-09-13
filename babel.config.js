81c11f6aa32a85ec2a74e44709916da8fb3eb873
Ignacio Olaciregui <ignacioola@gmail.com>
2022-09-13 10:00:44 -0300

    Remove comments

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
-// module.exports = {
-//   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
-// }
-// module.exports = { presets: ['@babel/preset-env'] }
 module.exports = {
   presets: ['next/babel'],
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,11 +6,9 @@ module.exports = {
   solidity: '0.8.9',
   networks: {
     hardhat: {
-      chainId: 4,
-      // chainId: 31337,
+      chainId: 31337,
       forking: {
         url: 'https://rinkeby.infura.io/v3/674b516cea814fc79d06e5e917ba1a11',
-        // blockNumber: 4135053,
       },
     },
   },
